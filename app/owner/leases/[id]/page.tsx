537c7e0654c54d019cd87e89680aebbff56e7592
Valerian5 <valerian.joubert.pro@gmail.com>
2025-09-29 17:52:16 +0200

    maj

--- app/owner/leases/[id]/page.tsx
+++ app/owner/leases/[id]/page.tsx
@@ -710,16 +710,6 @@ export default function LeaseDetailPage() {
                   </div>
                 </div>
 
-                {/* Bouton de téléchargement du bail signé quand les deux signatures sont présentes */}
-                {ownerIsSigned && tenantIsSigned && (
-                  <div className="pt-4">
-                    <Button variant="outline" asChild>
-                      <a href={`/api/leases/${leaseId}/download-signed-document`} target="_blank" rel="noopener noreferrer">
-                        <Download className="h-4 w-4 mr-2" /> Télécharger le bail signé
-                      </a>
-                    </Button>
-                  </div>
-                )}
               </CardContent>
             </Card>
           </TabsContent>
@@ -764,6 +754,17 @@ export default function LeaseDetailPage() {
                   </div>
                 </CardContent>
               </Card>
+            )}
+
+            {/* Bouton de téléchargement du bail signé quand les deux signatures sont présentes */}
+            {ownerIsSigned && tenantIsSigned && (
+              <div>
+                <Button variant="outline" asChild>
+                  <a href={`/api/leases/${leaseId}/download-signed-document`} target="_blank" rel="noopener noreferrer">
+                    <Download className="h-4 w-4 mr-2" /> Télécharger le bail signé
+                  </a>
+                </Button>
+              </div>
             )}
 
           {/* Section Signatures */}
